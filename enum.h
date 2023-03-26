4534d644d4ecff1e14acc7c87c039be0af0526ee
KamilSurlas <92538790+KamilSurlas@users.noreply.github.com>
2023-03-26 10:56:00 +0200

    file with enum classes

---
+++ enum.h
@@ -0,0 +1,13 @@
+#ifndef ENUM_H
+#define ENUM_H
+enum class rodzajWspomnienia{
+    Smutne = 0,
+    Neutralne = 1,
+    Radosne = 2
+};
+enum class kierunekPrzewijania{
+    NaDol,
+    WGore
+};
+
+#endif // ENUM_H
